2f9a8a49278a5933538807e15b506cea5541ab5d
Amresh Tripathy <amreshtripathy184@gmail.com>
2021-06-12 22:36:08 +0530

    Add files via upload

---
+++ 10_mem.java
@@ -0,0 +1,12 @@
+class mem
+{
+	public static void main(String args[])
+	{
+	 byte a=8,b=5,c;
+	 System.out.println(a&b);
+	 System.out.println(a^b);
+	 System.out.println(a|b);
+	 System.out.println(a<<14);
+	 System.out.println(~a);
+	}
+}
